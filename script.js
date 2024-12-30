e1a00ad781db32104fce71a23cf32e2da1c150cf
Yash pareek <134516657+yashpp221@users.noreply.github.com>
2024-12-30 18:54:41 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,6 @@
-// script.js
-document.addEventListener("mousemove", (e) => {
-  const mouseX = e.clientX / window.innerWidth;
-  const mouseY = e.clientY / window.innerHeight;
+const menuToggle = document.querySelector('.menu-toggle');
+const menu = document.querySelector('.menu');
 
-  document.body.style.setProperty("--mouseX", mouseX);
-  document.body.style.setProperty("--mouseY", mouseY);
-});
-// script.js
-const menuToggle = document.querySelector(".menu-toggle");
-const menu = document.querySelector(".menu");
-
-menuToggle.addEventListener("click", () => {
-  menu.classList.toggle("show");
+menuToggle.addEventListener('click', () => {
+  menu.classList.toggle('menu-active');
 });
